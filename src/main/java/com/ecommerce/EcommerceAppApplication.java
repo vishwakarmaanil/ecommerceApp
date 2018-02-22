da80b49f8f511e7ba5195a1365426ad678a30c2e
Anil Vishwakarma <vishwakarmaanil321@gmail.com>
2018-02-22 16:41:08 -0600

    Initial Commit

---
+++ src/main/java/com/ecommerce/EcommerceAppApplication.java
@@ -0,0 +1,12 @@
+package com.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceAppApplication.class, args);
+	}
+}
